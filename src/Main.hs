6a4e02bfbca4ec909f81066096ec27c129fecdc3
Vincent <vpark45@gmail.com>
2022-06-07 12:58:04 +0000

    Step Three: Making a puzzle

--- src/Main.hs
+++ src/Main.hs
@@ -40,6 +40,60 @@ randomWord wl = do
 randomWord' :: IO String
 randomWord' = gameWords >>= randomWord
 
+data Puzzle = Puzzle String [Maybe Char] [Char]
+
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $
+      fmap renderPuzzleChar discovered)
+   ++ " Guessed so far: " ++ guessed
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle word = Puzzle word nothings []
+  where nothings = const Nothing <$> word
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle word _ _) c = c `elem` word
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle word _ guessed) c = c `elem` guessed
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar Nothing  = '_'
+renderPuzzleChar (Just x) = x
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c =
+  Puzzle word newFilledInSoFar (c : s)
+  where zipper guessed wordChar guessChar = if wordChar == guessed
+                                            then Just wordChar
+                                            else guessChar
+        newFilledInSoFar = zipWith (zipper c) word filledInSoFar
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+
+  putStrLn $ "Your guess was: " ++ [guess]
+  case ( charInWord puzzle guess
+       , alreadyGuessed puzzle guess) of
+
+    (_, True) -> do
+      putStrLn "You already guessed that\
+              \ character, pick \
+              \ something else!"
+      return puzzle
+
+    (True, _) -> do
+      putStrLn "This character was in the\
+              \ word, filling in the word\
+              \ accordingly"
+      return (fillInCharacter puzzle guess)
+
+    (False, _) -> do
+      putStrLn "This character wasn't in\
+              \ the word, try again."
+      return (fillInCharacter puzzle guess)
+
 main :: IO ()
 main = do
   putStrLn "hello world"
