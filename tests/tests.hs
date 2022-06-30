d73ab21a2fdf06cf761583ffcb343561c1765202
Vincent <vpark45@gmail.com>
2022-06-30 14:10:28 +0000

    Test handleGuess function

--- tests/tests.hs
+++ tests/tests.hs
@@ -5,12 +5,16 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
+  let puzzle = freshPuzzle "hello"
   describe "fillInCharacter" $ do
     it "fillInCharacter" $ do
-      fillInCharacter (freshPuzzle "hello") 'h'
+      fillInCharacter puzzle 'h'
       `shouldBe`
       Puzzle "hello" [ Just 'h'
                      , Nothing
                      , Nothing
                      , Nothing
                      , Nothing ] "h"
+    it "handleGuess" $ do
+      p <- handleGuess puzzle 'h'
+      p `shouldBe` p
